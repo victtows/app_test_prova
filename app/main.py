711e6e1f7d9f2f13f88e872f63f649362362c871
victtows <josevictorjustinosilva@gmail.com>
2025-05-29 09:26:28 -0300

    feat: :sparkles: Adicionando o fastapi no main.py

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/square/{x}")
+def square(x: int):
+    return {"result": x + x}  # Erro proposital aqui
+
+@app.get("/double/{x}")
+def double(x: int):
+    return {"result": x * 2}
